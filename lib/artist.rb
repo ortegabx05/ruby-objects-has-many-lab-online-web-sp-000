1cecf028fd9e03e7fd2a5aead90dbbb4d3221b1f
jessi ortega <ortegabx05@gmail.com>
2019-08-03 22:13:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
-
-  attr_accessor :name, :songs
+  
+  attr_accessor :name
 
   @@song_count = 0
 
@@ -8,27 +8,25 @@ class Artist
     @name = name
     @songs = []
   end
-  
-  def songs
-    @songs 
-  Song.all.select {|song| song.artist == self}
-  end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
   end
 
   def self.song_count
-    @@song_count += 1
+    @@song_count
   end
-
 end
