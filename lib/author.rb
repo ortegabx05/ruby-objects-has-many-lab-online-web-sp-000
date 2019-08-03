6a6f0c0e54da15018807e72d7b81f05eb067fc51
jessi ortega <ortegabx05@gmail.com>
2019-08-03 22:05:41 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,17 +1,12 @@
-class Author 
-  
-  attr_accessor :name
-  
-  @@post_count = 0
+class Author
 
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+  
   def initialize(name)
     @name = name
     @posts = []
-  end
-
-  def posts
-    @posts
-    Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
@@ -22,16 +17,12 @@ class Author
 
   def add_post_by_title(title)
     post = Post.new(title)
-    self.posts << post
+    @posts << post
     post.author = self
     @@post_count += 1
   end
 
   def self.post_count
     @@post_count += 1
-  end
-    
-  def self.post_count
-    @@post_count
   end
 end
